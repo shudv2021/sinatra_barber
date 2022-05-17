08e70ca86701a599765c19b54779a6825afaf3ff
dmitry <shudv@yandex.ru>
2022-05-17 14:59:11 +0300

    add_save_to_file

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School!!!!</a>"			
@@ -16,4 +17,15 @@ end
 
 get '/visit' do
 	erb :visit
+end
+
+post '/visit' do
+	add_to_clienlist(params[:username], params[:phone], params[:time], params[:barber])
+erb :visit
+end
+
+def add_to_clienlist(name, phone, time, barber)
+	output = File.open './public/clientlist.txt', 'a'
+	output.write "Client #{name}, contact number: #{phone}, will be on #{time} to #{barber}\n"
+	output.close
 end
