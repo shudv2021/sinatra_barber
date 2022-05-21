015c6ea345799286244ecd77d35ca03036f5928d
dmitry <shudv@yandex.ru>
2022-05-21 09:28:42 +0300

    check work error function

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra/reloader'
 require 'pry'
 
 get '/' do
+	@error = "something wrong!"
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School!!!!</a>"			
 end
 
