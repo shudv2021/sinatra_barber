e78c2ea79c012b9464b2ab985942b7c3a053ef3e
dmitry <shudv@yandex.ru>
2022-05-26 14:33:39 +0300

    clearn / method

--- app.rb
+++ app.rb
@@ -21,7 +21,6 @@ configure do
 end
 
 get '/' do
-	@error = "something wrong!"
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School!!!!</a>"			
 end
 
